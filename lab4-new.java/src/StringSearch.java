3fc61ff23497d3c93afb114a0e5a8c229c46c35c
FriedDonkey <frida@sedata.org>
2024-12-03 10:43:54 +0100

    added code for searching for a string in a txt-file and a test file called cats.txt

---
+++ lab4-new.java/src/StringSearch.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+public class StringSearch {
+
+    public static ArrayList<String> search(String word, File file) {
+
+        ArrayList<String> matches = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+    
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if(checkLine(word, line) != null) {
+                    matches.add(line);
+                }
+            }
+    
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return matches;
+    }
+
+
+    public static String checkLine(String word, String line) {
+        if(line.contains(word)) {
+            return line;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(args[0], new File(args[1])));
+	}
+
+}
